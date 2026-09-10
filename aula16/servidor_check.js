3a14475b5c1bdf6e8542092901b54fcc2aa6ef6e
Daniel Torres Santos <daniel.t.santos@edu.senai.br>
2026-09-10 13:33:32 -0300

    Adição do script ps aux | grep node no arquivo processos.log

--- aula16/servidor_check.js
+++ aula16/servidor_check.js
@@ -14,5 +14,5 @@ app.get('/api/v1/status-servidor', (req, res) => {
 });
 
 app.listen(PORT, () => {
-	console.log(`[Binario Tech] Servidor de Validação da Aula 16 ativo na porta ${PORT}`);
+	console.log(`[Binario Tech] SERVIDOR DE VALIDAÇÃO DA AULA 16 ATIVO NA PORTA ${PORT}`);
 });
